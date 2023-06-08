982af0875990c100f81679cb1eb7b26366d40552
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2023-06-08 12:16:54 +0530

    Add files via upload

---
+++ maths.sh
@@ -0,0 +1,15 @@
+echo "enter the number"
+read a
+echo "enter the number"
+read b
+n=$((a+b))
+echo  sum is $n
+n2=$((a-b))
+echo sub is $n2
+n3=$((a*b))
+echo mul is $n3
+
+n3=$((a/b))
+echo div is $n4
+
+
